7986708c9938b3a92cc2d3ce0016e474c572a9f6
Tinus <martijnqvtz@outlook.com>
2024-04-08 09:25:49 +0200

    Renamed the code file from clean.R to modelfit.R

--- code/modelfit.R
+++ code/modelfit.R
@@ -3,9 +3,11 @@
 # Author: Martijn van 't Zelfden
 # Project: Modeling time series data
 #
+# Description: First try at modeling time series data using parsnip for model
+# development and modeltime to work with timeseries data
 ################################################################################
 
-# 1. Importing libraries -------------------------------------------------------
+# 1. Importing libraries ----
 
 library(tidyverse) # Foundational packages, such as dplyr and ggplot
 library(tidymodels) # Package for modeling data
@@ -14,12 +16,12 @@ library(lubridate)
 library(modeltime)
 tidymodels_prefer() # Prefers tidymodel function names over conflicting names
 
-# 2. Importing the dataset -----------------------------------------------------
+# 2. Importing the dataset ----
 setwd("~/1_r/modelingtimeseries/code")
 
 df <- as_tibble(readRDS("../data/sales.RDS"))
 
-# 3. Exploratory data analysis -------------------------------------------------
+# 3. Data preparation  ----
 
 soldMost <- names(which.max(table(df$artikelcode)))
 
@@ -38,6 +40,7 @@ data <- df |>
   summarise(aantal = sum (aantal)
             )
 
+
 splits <- time_series_split(
   data,
   assess = "3 months",
@@ -48,25 +51,57 @@ splits |>
   tk_time_series_cv_plan() |> 
   plot_time_series_cv_plan(datum, aantal)
 
+# Training the prophet model -----
+
 model_prophet <- 
-  prophet_reg(seasonality_yearly = TRUE
+  prophet_reg(seasonality_yearly = TRUE, 
+              seasonality_daily = FALSE # Disabled because row is sum of day
               ) |> 
   set_engine("prophet"
              ) |> 
   fit(aantal ~ datum, training(splits)
   )
 
+# Training the GLMNET model -----
+
 model_glmnet <- linear_reg(penalty = 0.01) |> 
   set_engine("glmnet") |> 
   fit(
-    aantal ~ wday(datum, label = TRUE)
-    + month(datum, label = TRUE)
-    + as.numeric(datum),
+    aantal ~ wday(datum, label = TRUE) 
+    + month(datum, label = TRUE), # Adding "month" as additional predictor
     training(splits)
   )
 
+# Model time table ----
 model_tbl <- modeltime_table(
   model_prophet,
   model_glmnet
 )
 
+# Predictions and residuals ----
+calib_tbl <- model_tbl |> 
+  modeltime_calibrate(testing(splits))
+
+# Model Accuracy ----
+calib_tbl |> ?modeltime_accuracy() # Truly terrible scores
+
+# Visualization ----
+calib_tbl |> 
+  modeltime_forecast(
+    new_data = testing(splits),
+    actual_data = data
+  ) |> 
+  plot_modeltime_forecast()
+
+# Forecasting ----
+future_forecast_tbl <- calib_tbl |> 
+  modeltime_refit(data) |> 
+  modeltime_forecast(
+    h = "6 months",
+    actual_data = data
+  )
+
+# Plotting the forecast ----
+future_forecast_tbl |> 
+  plot_modeltime_forecast()
+  
