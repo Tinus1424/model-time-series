eb2a01f7086e1b73b23cdac6f3d3d02bf9738293
Tinus <martijnqvtz@outlook.com>
2024-04-08 10:27:05 +0200

    data split experimentation

--- code/datasplit.R
+++ code/datasplit.R
@@ -6,12 +6,37 @@
 # Description: Practicing and viewing the effect of splits
 ################################################################################
 
-setwd("~/1_r/modelingtimeseries/code")
-
-library(tidyverse)
+# Environment preparation ----
+source("code/dataprep.R")
 library(tidymodels)
 library(timetk)
+tidymodels_prefer()
 
-df <- as_tibble(readRDS("../data/sales.RDS"))
+# Exploring ----
+ggplot(data, aes(x = aantal)) +
+  geom_histogram()
 
-?source("modelfit.R")
+# Splitting ----
+splitweird <- initial_split(data)
+
+# Splitting this way is not good because the training data does not replicate
+# the original pattern, timeseries should be kept together in a split because
+# of time itself is a component that captures information and spills to neighbors
+
+splitweird |> 
+  tk_time_series_cv_plan() |> 
+  plot_time_series_cv_plan(datum, aantal)
+
+
+split <- time_series_split(
+  data,
+  assess = "3 months",
+  cumulative = TRUE
+)
+
+
+split |> 
+  tk_time_series_cv_plan() |> 
+  plot_time_series_cv_plan(datum, aantal)
+
+?timetk
